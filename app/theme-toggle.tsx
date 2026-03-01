ad36a1b17909c307911eea26def4ce426d036fe3
Md Tasrif Khan <tasrif.k.56@gmail.com>
2026-03-02 01:54:30 +0600

    fully fixed dark mode

--- app/theme-toggle.tsx
+++ app/theme-toggle.tsx
@@ -8,7 +8,7 @@ export function ThemeToggle(){
     const { theme, setTheme } = useTheme();
     return (
         <Button variant="outline" size="icon" className="rounded-full" onClick={() => setTheme( theme === "light" ? "dark" : "light")}>
-            <Sun className="absolute h-10 w-10 rotate-0 scale-100 dark:-rotate-90 dark:scale-0">
+            <Sun className="absolute text-black h-10 w-10 rotate-0 scale-100 dark:-rotate-90 dark:scale-0">
             </Sun>
             <Moon className="absolute h-10 w-10 rotate-90 scale-0 dark:rotate-0 dark:scale-100"></Moon>
         </Button>
--- components/Home/HomePage.tsx
+++ components/Home/HomePage.tsx
@@ -19,31 +19,7 @@ const HomePage = () => {
       </div>
       
       <div className="items-center justify-center">
-        <h4 className="font-bold">
-          <div>
-          <Button>Dark Mode</Button>
-          <Button variant="secondary">Light Mode</Button>
-          </div><ThemeToggle/>
-          {/* <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => setTheme("light")}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("dark")}>
-                Dark
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("system")}>
-                System
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu> */}
+        <h4 className="font-bold"> 
           </h4>
       </div>
   </div>)
