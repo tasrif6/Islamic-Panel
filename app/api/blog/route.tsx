f6d493ca2f68fe1d2b59da5cd5e52a388f8ed066
Md Tasrif Khan <tasrif.k.56@gmail.com>
2026-04-07 00:34:51 +0600

    api adding

--- app/api/blog/route.tsx
+++ app/api/blog/route.tsx
@@ -1,6 +1,17 @@
+import DBConnect from "@/lib/db"
 import { NextResponse } from "next/server"
 
+const LoadDB = async () => {
+    await DBConnect()
+    console.log("Database connected")
+    return NextResponse.json({msg: "Database Connected"}), {status: 200}
+}
 export async function GET(request) {
-    console.log("Blog GET Hit")
+
     return NextResponse.json({msg: "API Working"})
+}
+
+export async function POST(request) {
+    const formData = await request.formData()
+    const timestamp = Date.now()
 }
